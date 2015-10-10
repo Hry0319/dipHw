ff48a6aed9560b07ff05818b089e921beb1a12ef
JayHsieh <k3331863@gmail.com>
2015-10-11 00:19:13 +0800

    numpy

--- untitled1.py
+++ untitled1.py
@@ -7,6 +7,7 @@ Created on Sat Oct 10 16:02:03 2015
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+from pylab import *
 
 
 #img = cv2.imread ("lena.bmp",1)
@@ -22,3 +23,41 @@ from matplotlib import pyplot as plt
 #plt.show()
 
 
+img = cv2.imread('81.png',cv2.IMREAD_ANYCOLOR)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB);
+
+width = img.shape[1]
+height = img.shape[0]
+x0 = img.shape[1]/2
+y0 = img.shape[0]/2
+
+
+
+
+
+
+def bilinear_interpolate(im, x, y):
+    x = np.asarray(x)
+    y = np.asarray(y)
+
+    x0 = np.floor(x).astype(int)
+    x1 = x0 + 1
+    y0 = np.floor(y).astype(int)
+    y1 = y0 + 1
+
+    x0 = np.clip(x0, 0, im.shape[1]-1);
+    x1 = np.clip(x1, 0, im.shape[1]-1);
+    y0 = np.clip(y0, 0, im.shape[0]-1);
+    y1 = np.clip(y1, 0, im.shape[0]-1);
+
+    Ia = im[ y0, x0 ]
+    Ib = im[ y1, x0 ]
+    Ic = im[ y0, x1 ]
+    Id = im[ y1, x1 ]
+
+    wa = (x1-x) * (y1-y)
+    wb = (x1-x) * (y-y0)
+    wc = (x-x0) * (y1-y)
+    wd = (x-x0) * (y-y0)
+
+    return wa*Ia + wb*Ib + wc*Ic + wd*Id
