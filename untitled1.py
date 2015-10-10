33a82d5970433f6eebd016f77d7f7aa10f414748
JayHsieh <k3331863@gmail.com>
2015-10-10 22:38:29 +0800

    DIP homework with python

---
+++ untitled1.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 10 16:02:03 2015
+
+@author: k3331863
+"""
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+#img = cv2.imread ("lena.bmp",1)
+#cv2.imshow('image',img)
+#cv2.waitKey(0)
+#cv2.destroyAllWindows()
+
+
+#img = cv2.imread('lenaColor.bmp',cv2.IMREAD_ANYCOLOR)
+#img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB);
+#plt.imshow(img, cmap = 'gray', interpolation = 'bicubic')
+##plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+#plt.show()
+
+
