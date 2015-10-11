157dbdb3238a2168d9e03d7873983d25ad981cce
JayHsieh <k3331863@gmail.com>
2015-10-11 11:31:26 +0800

    matrix ok

--- untitled1.py
+++ untitled1.py
@@ -31,33 +31,11 @@ height = img.shape[0]
 x0 = img.shape[1]/2
 y0 = img.shape[0]/2
 
+maxRow = math.sqrt(img.shape[0]**2 + img.shape[1]**2)
+maxCol = maxRow
+#print maxCol    #1347.62494782
+
+scale1 = np.zeros(shape=(maxRow+1,maxCol+1))
+#print scale1.shape
 
 
-
-
-
-def bilinear_interpolate(im, x, y):
-    x = np.asarray(x)
-    y = np.asarray(y)
-
-    x0 = np.floor(x).astype(int)
-    x1 = x0 + 1
-    y0 = np.floor(y).astype(int)
-    y1 = y0 + 1
-
-    x0 = np.clip(x0, 0, im.shape[1]-1);
-    x1 = np.clip(x1, 0, im.shape[1]-1);
-    y0 = np.clip(y0, 0, im.shape[0]-1);
-    y1 = np.clip(y1, 0, im.shape[0]-1);
-
-    Ia = im[ y0, x0 ]
-    Ib = im[ y1, x0 ]
-    Ic = im[ y0, x1 ]
-    Id = im[ y1, x1 ]
-
-    wa = (x1-x) * (y1-y)
-    wb = (x1-x) * (y-y0)
-    wc = (x-x0) * (y1-y)
-    wd = (x-x0) * (y-y0)
-
-    return wa*Ia + wb*Ib + wc*Ic + wd*Id
